00f88b098f398daa4ace4a922fe63d3ae98c8140
Zephyruze <zeph.evolution@gmail.com>
2014-03-29 01:28:33 +0800

    New Foobar

---
+++ foobar.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+<html>
+<head>
+   <title>Hello World</title>
+   <meta http-equiv="content-type" content="text/html"; charset="utf-8" /> 
+</head>
+<style>
+   h1 {
+     
+   }
+</style>
+<body>
+  <h1>Foo Bar</h1>
+</body>
+</html>
